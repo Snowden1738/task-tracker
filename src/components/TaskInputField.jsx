43cf54dd77a4ae430ca155c6360da0d22ba4d539
Arkajyoti Banerjee <arka.jyotib2000@gmail.com>
2021-09-05 01:25:36 +0530

    Indented the contents of the wrapper div of TaskInputField

--- src/components/TaskInputField.jsx
+++ src/components/TaskInputField.jsx
@@ -15,15 +15,15 @@ const TaskInputField = ({
 
 	return (
 		<div className={className}>
-		<label>{label}</label>
-		<input
-			type={type}
-			value={value}
-			placeholder={placeholder}
-			onChange={(e) => onChange(e)}
-		>
-		</input>
-		<h6>{ prompt && value.length == 0 ? `${label} cannot be empty!` : `` }</h6>
+			<label>{label}</label>
+			<input
+				type={type}
+				value={value}
+				placeholder={placeholder}
+				onChange={(e) => onChange(e)}
+			>
+			</input>
+			<h6>{ prompt && value.length == 0 ? `${label} cannot be empty!` : `` }</h6>
 		</div>
 	)
 }
